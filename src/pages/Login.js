b29356c1c6f384367b53c6c34f53d609c3d56a96
ningaraj <nb123@gmail.com>
2023-06-18 18:55:37 +0530

    Login as guest

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -22,9 +22,24 @@ const authDetail = {
  }catch(error) {
   toast.error(error.message, {theme:"dark", position:"bottom-right", autoClose:5000, closeOnClick:true, closeButton:"true"})
 }
-    
-
+  } 
+async function handleLoginGuest(){
+  email.current.value="blacky@gmail.com";
+  password.current.value="123456789";
+  try{
+  const authDetail = {
+      email: email.current.value,
+      password: password.current.value,
+    };
+  const data =await login(authDetail);
+  data.accessToken ? navigate('/products') : toast.error(data);
+  } catch(error){
+  toast.error(error.message, {theme:"dark", position:"bottom-right", autoClose:5000, closeOnClick:true, closeButton:"true"})
   }
+  
+}
+
+  
   return (
     <main>
       <section>
@@ -72,7 +87,7 @@ const authDetail = {
           Log In
         </button>
       </form>
-      {/* <button className="mt-3 cursor-pointer text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-3 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login As Guest</button> */}
+      <button onClick={handleLoginGuest} className="mt-3 cursor-pointer text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-3 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Login As Guest</button>
     </main>
   );
 };
