5b26d8fba564e64aaaf7bb5b52f29df7fa47f3be
NingarajYalameli <126946919+NingarajYalameli@users.noreply.github.com>
2023-08-07 13:47:25 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@
 //Im updating again this file............
 //I m updating again and again.....
 //again im updating the file
+//hi i m ningaraj yalameli
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   darkMode: 'class',
