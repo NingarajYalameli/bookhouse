8c26addd53988e06ed5bd2409f149d655b275c0b
NingarajYalameli <126946919+NingarajYalameli@users.noreply.github.com>
2023-08-07 11:41:46 +0530

    Update App.js231?2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import { Footer, Header } from "./components";
 import { AllRoutes } from "./routes/AllRoutes";
-
+//This is a main file....
 function App() {
   return (
     <div className="dark:bg-slate-800">
