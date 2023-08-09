bf95a57af2fa315126a3a81ae172ce2013119a40
NingarajYalameli <126946919+NingarajYalameli@users.noreply.github.com>
2023-08-09 07:42:06 +0530

    added comment in App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 import { Footer, Header } from "./components";
 import { AllRoutes } from "./routes/AllRoutes";
-
+//This is the entry point for all components
 function App() {
   return (
     <div className="dark:bg-slate-800">
